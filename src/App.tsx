43929b24593d204c4732625a63b52d4b2ff3c39a
julysNICK <julystube@gmail.com>
2022-09-12 21:14:32 -0300

    fix erro styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './app.css';
 import Course from './components/course';
 import { Navbar } from './components/header';
+import { Home } from './components/home';
 function App() {
   return (
     <div className="body_complete">
       <Navbar />
+      <div className="container-home">
+        <Home />
+      </div>
       <Course />
     </div>
   );
