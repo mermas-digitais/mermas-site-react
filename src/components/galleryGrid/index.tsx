c9038f4c1edaca2e6278125cbcfd2246dd53b7df
julysNICK <julystube@gmail.com>
2022-11-02 19:20:08 -0300

    add new section gallery grid

--- src/components/galleryGrid/index.tsx
+++ src/components/galleryGrid/index.tsx
@@ -9,6 +9,22 @@ export default function GalleryGrid() {
             <h1 className="blackPart">Galeria das</h1>{' '}
             <h1 className="pinkPart">Mermãs Digitais </h1>
           </div>
+          <p>
+            Diga Xis! E vem ver pra galeria das Mermãs Veja o que nás anda
+            aprontando ;D
+          </p>
+        </div>
+        <div className="input_box">
+          <div className="box_box">
+            <input
+              type="text"
+              placeholder="Pesquisar"
+              className="seacrhInput"
+            />
+            <button className="buttonSearch">
+              <img src="./image/MagnifyingGlass.png" alt="Ícone de foguete" />
+            </button>
+          </div>
         </div>
       </div>
 
