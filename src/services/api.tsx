563c9d6fcc5d170671793341596bf9dfc38d747b
laillagaleno <laillagalenoitz@gmail.com>
2023-05-15 11:43:15 -0300

    api

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: '/api'
+    baseURL: 'https://api-mermas.onrender.com/api'
 });
 export default api;
--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,17 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '',
-    port: 3333,
-    strictPort: true,
-    proxy: {
-      '/api': {
-        target: 'https://api-mermas.onrender.com/api',
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+  // server: {
+  //   host: '',
+  //   port: 3333,
+  //   strictPort: true,
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://api-mermas.onrender.com/api',
+  //       changeOrigin: true,
+  //       secure: true,
+  //       rewrite: (path) => path.replace(/^\/api/, ''),
+  //     },
+  //   },
+  // },
 });
